2e7db950c39e0af4b25bd4f2c4a8844901dc562d
piet <pprotassow@posteo.de>
2022-10-20 13:56:14 +0200

    types

---
+++ python-basic/datatypes/main.py
@@ -0,0 +1,11 @@
+# print(type(1))
+# print(type(type(1)))
+# print(isinstance('s', str))
+# print(isinstance(1+1j, complex))
+# print(type(1j))
+
+# print('str', 'another', 'etc')
+print('str', 'another', 'etc', sep=';')
+
+print(type(type(1)))
+isinstance(1, int)
