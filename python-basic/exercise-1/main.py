950058d77fb9557b7f015c3b6ce00f4205f95a94
piet <pprotassow@posteo.de>
2022-10-20 13:55:24 +0200

    python-1 exercise

---
+++ python-basic/exercise-1/main.py
@@ -0,0 +1,53 @@
+#####Task1###
+
+a=234
+b=43.12
+c=3+2j
+d='Hello'
+e=False
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# print(a, b, c, d, e, sep='\n')
+
+print(' ')
+########Task2####
+
+print(a, 'is type of', type(a))
+print(b, 'is type of', type(b))
+print(c, 'is type of', type(c))
+print(d, 'is type of', type(d))
+print(e, 'is type of', type(e))
+
+print(' ')
+######Task3#####
+
+print(isinstance(a, int))
+print(isinstance(b, int))
+print(isinstance(c, complex))
+print(isinstance(d, str))
+print(isinstance(e, float), end='\n\n')
+
+# print('bool in number', bool(0))
+
+# print(' ')
+
+######Task4#####
+
+# Printing out stuff
+# printing ininstance
+
+print('Is', a, 'an instance of int?:', isinstance(a, int)) # print a integer
+print('Is', b, 'an instance of int?:', isinstance(b, int)) 
+print('Is', c, 'an instance of int?:', isinstance(c, complex))
+print('Is', e, 'an instance of int?:', isinstance(e, bool))
+print('Is', d, 'an instance of int?:', isinstance(d, int))
+
+"""
+Hello world
+again
+"""
