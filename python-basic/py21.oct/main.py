3f4e4a3a725f7053af1300873b5b13c37a61d989
piet <pprotassow@posteo.de>
2022-10-21 16:37:09 +0200

    Control Flow

---
+++ python-basic/py21.oct/main.py
@@ -0,0 +1,160 @@
+#sequential statments
+a = 5
+b = 2
+sum = 5 + 2
+# print(sum)
+
+# decision statement
+
+#############
+x = 2
+
+
+# else:
+#     print('x is smaller then 1')
+####################
+
+######################
+y = 8
+
+# if y > 9:
+#     print('y is greater then 9')
+# else:
+    # print('condition not meet')
+
+# print('Hello world')
+
+##########################
+
+
+###################
+### short if###
+a = 6
+# if a > 5: print("a is greater than 5")
+
+## same as the code below
+
+# if a > 5:
+#     print("a is greater than 5")
+
+
+# print('Hello world')
+
+###################
+
+#################
+###elif##########
+
+a = 8
+b = 20
+
+# if a > 9:
+#     print('a is greater than 9')
+# elif b > 19:
+#     print('b is greater than 10')
+# else:
+#     print('Neither a is greater than 9 nor b is greater than 19')
+
+y = 10
+x = 18
+
+# if y > 9:
+#     print('y is greater than 9')
+#     if x > 19:
+#         print('x is greater than 19')
+#     else:
+#         print('y is greater than 9 but x is not greater than 19')
+# else:
+#     print('y is not great than 9')
+
+
+# if y == 10:
+#     print('y == 10')
+#     if x != 19:
+#         print(' x != 19')
+#     else:
+#         print('y is greater than 9 but x is not greater than 19')
+# else:
+#     print('y is not great than 9')
+
+# dummy = 2.0
+# dummy = 'hello world'
+
+# if type(dummy) == float:
+#     print('yes, its a float')
+#     if dummy >= 2.0:
+#         print('dummy greater than or is equal to 2.0')
+# elif type(dummy) == str:
+#     print('yes it\'s a string')
+#     if dummy == 'hello world':
+#         print('dummy holds the string value "hello world"')
+
+
+################
+## repetition sequence
+## while loop
+
+# x=0
+
+SUM = 0
+
+# while x < 10:
+#     print(x)
+#     y = x + 1
+#     x = y
+
+# print('outside loop')
+
+    
+# for x in range(1, 10):
+#     print(x)
+
+
+# for x in range(0,10):
+#     print(x)
+#     if x == 3:
+#         print('x is 3 now')
+#     else:
+#         print('x is not 3')
+
+# for x in range(0,10):
+#     print(x)
+#     if type(x/2) != float:
+#         print('x is even')
+#     else:
+#         print('x is odd')
+# import math
+
+# r = input('Give me the radius of a sphere: ')
+# print(r)
+
+# if r.isnumeric():
+#     r = int(r)
+#     print(r)
+# else:
+#     r = input('Wrong, Give me the radius of a sphere: ')
+
+# r = int(r)
+
+# r = input('Give me the radius of a sphere: ')
+
+# while not r.isnumeric():
+#     r = input('Wrong, please give me the right data type: ')
+
+# r = int(r)
+
+
+
+
+# r = int(x)  # the interpreter throws an error and stops 
+            # stops the program
+
+
+# volume_sphere = 4/3 * math.pi * r ** 3
+
+# print('volume: ', volume_sphere)
+
+
+
+# for i in range(0, 10):
+#     print(i)
