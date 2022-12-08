df1175b825724f4c3cc01d5e0d3ef37a56357d1a
piet <pprotassow@posteo.de>
2022-12-08 22:39:23 +0100

    Refactor Collection Exercise 9:

--- python-basic/COLLECTIONS/ex9.py
+++ python-basic/COLLECTIONS/ex9.py
@@ -1,18 +1,7 @@
-def value_factory(index):
-    def decorator(func):
-        def inner(item):
-            return func(item)[index]
-        return inner
-    return decorator
-
-@value_factory(1)
-def sort_helper(item):
-    return item
-
 def count_characters(string):
     dict_char = {}
     for char in string.lower():
         dict_char[char] = string.lower().count(char)
-    return dict(sorted(dict_char.items(), key=sort_helper))
+    return dict(sorted(dict_char.items(), key=lambda x: x))
 
 print(count_characters("Elephant"))
