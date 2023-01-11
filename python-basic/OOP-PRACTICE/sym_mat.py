362062a811fbd273d1d1f9d45e2d367c496dd9a3
piet <pprotassow@posteo.de>
2023-01-11 23:24:48 +0100

    overloading

--- python-basic/OOP-PRACTICE/sym_mat.py
+++ python-basic/OOP-PRACTICE/sym_mat.py
@@ -1,4 +1,8 @@
 class SymMatrix:
     def __init__(self, components):
+        self.length = len(components)
+        isSymetric = (any(len(row) != self.length for row in components))
         self.components = components
         
+
+(1,2) + 1
