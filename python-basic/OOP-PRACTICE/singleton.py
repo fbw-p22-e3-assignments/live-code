f9efdb20861379025ef72821718fa04ee35ba0be
piet <pprotassow@posteo.de>
2023-01-12 16:00:29 +0100

    add submodule

--- python-basic/OOP-PRACTICE/singleton.py
+++ python-basic/OOP-PRACTICE/singleton.py
@@ -13,8 +13,27 @@ class Singleton:
 
 
 
-x = Singleton('x')
-y = Singleton('y')
-print(x.name)
-print(y.name)
+# x = Singleton('x')
+# y = Singleton('y')
+# print(x.name)
+# print(y.name)
+
+class Warehoues:
+    def __init__(self):
+        self._instance = self
+        self.emploees = 0
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(Warehoues, cls).__new__(cls)
+        cls._instance.emploees += 1
+        return cls._instance
+
+    def __str__(self):
+        return f"Number of employees: {self.emploees}"
+
+w = Warehoues()
+w2 = Warehoues()
+print(w)
+print(w2)
 
--- python-basic/OOP-PRACTICE/sym_mat.py
+++ python-basic/OOP-PRACTICE/sym_mat.py
@@ -1,8 +1,36 @@
 class SymMatrix:
     def __init__(self, components):
         self.length = len(components)
-        isSymetric = (any(len(row) != self.length for row in components))
-        self.components = components
+        isSymetric = all([len(row) == self.length for row in components])
+        print(isSymetric)
+        if isSymetric:
+            self.components = components
+        else:
+            raise ValueError("Not a symmetric matrix")
+
+    def __str__(self):
+         matrix = str("\n".join([ '('+' '.join([str(col) for col in row]) + ')' for row in self.components]))
+         return matrix
+
+    def shape(self):
+         return (self.length, self.length)
+
+    def __matmul__(self, other):
+         if self.shape()[1] != other.shape()[0]:
+             raise ValueError("Matrix dimensions do not match")
+         else:
+             pass
+             result = [[str(ele)+'A' for ele in rowX] for rowX in zip(self.components, other.transpose().components)]
+             return SymMatrix(result)
+
+    def transpose(self):
+        return SymMatrix(list(zip(*self.components)))
+    
         
 
-(1,2) + 1
+p1 = SymMatrix([[1, 2], [3, 4]])
+p2 = SymMatrix([[4, 5], [6, 5]])
+print(p1.transpose())
+print(p1 @ p2)
+
+
