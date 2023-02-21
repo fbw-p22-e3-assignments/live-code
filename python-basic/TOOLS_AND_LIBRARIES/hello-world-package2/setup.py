adbc57bd943db38cf13bd01e37f6c8e6ca1d16f3
piet <pprotassow@posteo.de>
2023-02-21 12:00:54 +0100

    packaging:done

--- python-basic/TOOLS_AND_LIBRARIES/hello-world-package2/setup.py
+++ python-basic/TOOLS_AND_LIBRARIES/hello-world-package2/setup.py
@@ -1,1 +1,10 @@
 import setuptools
+
+setuptools.setup(
+    name="hello-world-piet2",
+    version="0.0.1",
+    author="Piet",
+    description="prints hello world",
+    py_modules=['hello_world'],
+    package_dir={'': 'hello-world-piet2/src'}
+)
