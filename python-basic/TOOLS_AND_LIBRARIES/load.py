db28d6a455454fb8a03e3f64c11afdd6a5815827
piet <pprotassow@posteo.de>
2023-02-21 11:29:18 +0100

    start creating packages

--- python-basic/TOOLS_AND_LIBRARIES/load.py
+++ python-basic/TOOLS_AND_LIBRARIES/load.py
@@ -4,7 +4,11 @@
 # print(A)
 # print(pkg.module1)
 
-from path1 import namespace
-from path2 import namespace
-print(namespace.module1)
+# from path1.namespace import module1
+# from path2.namespace import module3
+# print(module1)
 # print(namespace.module3)
+
+from namespace import module1, module3
+print(module1)
+print(module3)
