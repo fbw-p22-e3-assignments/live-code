9785e906adfdead4e8bd28f511b526de70188675
piet <pprotassow@posteo.de>
2023-02-21 10:35:38 +0100

    regular pkg; namespace package

---
+++ python-basic/TOOLS_AND_LIBRARIES/load.py
@@ -0,0 +1,10 @@
+# from pkg import A
+# import pkg
+
+# print(A)
+# print(pkg.module1)
+
+from path1 import namespace
+from path2 import namespace
+print(namespace.module1)
+# print(namespace.module3)
--- python-basic/TOOLS_AND_LIBRARIES/pkg/__init__.py
+++ python-basic/TOOLS_AND_LIBRARIES/pkg/__init__.py
@@ -1,3 +1,5 @@
+print('HELLO WORLD')
 print(f'Invoking __init__.py for {__name__}')
 A = ['Hello', 'DCI', 'students']
-import pkg.mod1, pkg.mod2
+from pkg import mod1 as module1
+# import pkg.mod1, pkg.mod2
