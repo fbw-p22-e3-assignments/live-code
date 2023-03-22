7585dc736bfbe7447719b0f11cfb37ea08f84867
piet <pprotassow@posteo.de>
2023-03-22 16:26:17 +0100

    transactions

--- database/CONSISTENCY/consistency.sql
+++ database/CONSISTENCY/consistency.sql
@@ -10,7 +10,10 @@ insert into accounts(name, balance) values
     ('Columbus', 1000);
 
 begin;
+LOCK TABLE accounts IN ROW EXCLUSIVE MODE;
+
 update accounts set balance = balance - 100 where name='Newton';
 savepoint safe_users;
+LOCK TABLE accounts; -- default access exclusive
 update accounts set balance = balance + 100 where name='Columbus';
-commit;
+rollback to safe_users;
